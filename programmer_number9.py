d918ab823693f2b3315056e84772702729c1a9d5
Y0un9Ki <srilankakim66@gmail.com>
2023-12-25 01:38:17 +0900

    difficult

---
+++ programmer_number9.py
@@ -0,0 +1,48 @@
+# cards=[8,6,3,7,2,5,1,4]
+# list=[]
+# n=1
+# for i in str(cards[n-1]): #i=8 i=7 i=6 i=2
+#     if not n=True:
+#         break
+#     else:
+#         if i==True:
+#             # print(type(i))
+#             list+=cards[n-1] #+8 +7 +6 +2
+#             cards[n-1]=" " #8지워짐 7지워짐 6지워짐 2지워짐
+#             list+=cards[int(i)-1] #+4 +1 +5 +' '
+#             cards[int(i)-1]=" " #4지워짐 1지워짐 5지워짐
+#             n=cards[int(i)-1] #n=4 n=1 n=5 
+#         else:
+#             n+=1
+# list=[1,4,7,8]
+# cards=[' ',6,3,' ',2,5,' ',' ']
+
+
+a='n'
+b='m'
+print(a*2)
+result=[]
+result.append(a*2)
+result.append(b*4)
+print(result[0:])
+a=[1,2,3,4,5,6,7,8]
+print(a[::-1])
+
+
+
+
+
+
+
+
+
+# cards=[8,6,3,7,2,5,1,4]
+# list=[]
+# n=1
+# # print(type(cards[3]))
+# for i in str(cards[2]):
+#     print(i)
+#     print(2+int(i))
+    
+# n='1'
+# print(int(n)-1)
