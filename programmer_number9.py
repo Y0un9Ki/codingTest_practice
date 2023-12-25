290ff4b70774ed154f9b13b3a5e92efe704b38a7
Y0un9Ki <srilankakim66@gmail.com>
2023-12-25 23:55:19 +0900

    didnt understand

--- programmer_number9.py
+++ programmer_number9.py
@@ -1,32 +1,32 @@
-# cards=[8,6,3,7,2,5,1,4]
-# list=[]
-# n=1
-# for i in str(cards[n-1]): #i=8 i=7 i=6 i=2
-#     if not n=True:
-#         break
-#     else:
-#         if i==True:
-#             # print(type(i))
-#             list+=cards[n-1] #+8 +7 +6 +2
-#             cards[n-1]=" " #8지워짐 7지워짐 6지워짐 2지워짐
-#             list+=cards[int(i)-1] #+4 +1 +5 +' '
-#             cards[int(i)-1]=" " #4지워짐 1지워짐 5지워짐
-#             n=cards[int(i)-1] #n=4 n=1 n=5 
-#         else:
-#             n+=1
+cards=[8,6,3,7,2,5,1,4]
+list=[]
+n=1
+for i in str(cards): #i=8 i=7 i=6 i=2
+    if not n=True:
+        break
+    else:
+        if i==True:
+            # print(type(i))
+            list+=cards[n-1] #+8 +7 +6 +2
+            cards[n-1]=" " #8지워짐 7지워짐 6지워짐 2지워짐
+            list+=cards[int(i)-1] #+4 +1 +5 +' '
+            cards[int(i)-1]=" " #4지워짐 1지워짐 5지워짐
+            n=cards[int(i)-1] #n=4 n=1 n=5 
+        else:
+            n+=1
 # list=[1,4,7,8]
 # cards=[' ',6,3,' ',2,5,' ',' ']
 
 
-a='n'
-b='m'
-print(a*2)
-result=[]
-result.append(a*2)
-result.append(b*4)
-print(result[0:])
-a=[1,2,3,4,5,6,7,8]
-print(a[::-1])
+# a='n'
+# b='m'
+# print(a*2)
+# result=[]
+# result.append(a*2)
+# result.append(b*4)
+# print(result[0:])
+# a=[1,2,3,4,5,6,7,8]
+# print(a[::-1])
 
 
 
