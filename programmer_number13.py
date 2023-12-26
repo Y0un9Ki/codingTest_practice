f48f838120ff950cb4cf33165e0eb0f1f13ab0d0
Y0un9Ki <srilankakim66@gmail.com>
2023-12-26 20:09:58 +0900

    잘모르겠음

---
+++ programmer_number13.py
@@ -0,0 +1,14 @@
+list=[3,2,1,3,3,4,1,2,3,1]
+list_=[3,2,1,3,3,4,1,2,3,1]
+# print(min(list))
+n=0
+a=len(list_)
+list1=[]
+while n<a+1:
+    list1.append(min(list))
+    # print(min(list))
+    list.remove(min(list))
+    n+=1
+# list1.append(min(list))
+
+print(list1)
